532f6d56d1343e566164ea73feb9683a6aa34878
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-09-01 13:37:02 -0300

    Cadastro parte 6

--- src/component/general/Avatarinput.tsx
+++ src/component/general/Avatarinput.tsx
@@ -1,12 +1,18 @@
 import { Modal } from 'react-bootstrap'
 import { useState } from 'react'
-
 import avatarIcon from '../../assets/images/avatar.svg'
-//import avatar01 from '../../assets/objects/avatar/avatar_01_front.png'
 
-export const AvatarInput = () => {
+type AvatarInputProps = {
+  image: string
+  setImage(s: string): void
+}
+
+export const AvatarInput: React.FC<AvatarInputProps> = ({
+  image,
+  setImage
+}) => {
   const [showModal, setShowModal] = useState(false)
-  const [selected, setSelected] = useState(false)
+  const [selected, setSelected] = useState('')
 
   const avatars = [
     { value: 'avatar_01' },
@@ -20,12 +26,26 @@ export const AvatarInput = () => {
     { value: 'avatar_09' }
   ]
 
-  const getAvatarUrl = (avatar: string) => {
-    const path = `../../assets/objects/avatar/${avatar}_front.png`; // Caminho que especifica a localização da imagem com base na variável "avatar"
-    const imageUrl = new URL(path, import.meta.url); // Cria uma nova URL com base no caminho da imagem e na URL do módulo atual
-    return imageUrl.href; // Retorna a URL completa da imagem
-}
+  const avatarImage = () => {
+    if (image && image.trim().length > 0) {
+      const path = `../../assets/objects/avatar/${image}_front.png`
+      const imageUrl = new URL(path, import.meta.url)
+      return imageUrl.href
+    }
+    return avatarIcon
+  }
 
+  const getAvatarUrl = (avatar: string) => {
+    const path = `../../assets/objects/avatar/${avatar}_front.png` // Caminho que especifica a localização da imagem com base na variável "avatar"
+    const imageUrl = new URL(path, import.meta.url) // Cria uma nova URL com base no caminho da imagem e na URL do módulo atual
+    return imageUrl.href // Retorna a URL completa da imagem
+  }
+  const submitAvatar = () => {
+    if (selected) {
+      setImage(selected)
+    }
+    setShowModal(false)
+  }
 
   return (
     <>
@@ -34,7 +54,7 @@ export const AvatarInput = () => {
         onClick={() => setShowModal(true)}
       >
         <div className="avatar">
-          <img src={avatarIcon} alt=" avatar" className="avatar" />
+          <img src={avatarImage()} alt=" avatar" className="avatar" />
         </div>
       </div>
       <Modal
@@ -49,18 +69,31 @@ export const AvatarInput = () => {
             <div className="line" />
             <div className="avatars-scroll">
               <div className="avatars">
-               {avatars.map((avatar: any) =>
-               <div className={'container-avatar'} key={avatar.value}>
-                <img src={getAvatarUrl(avatar.value)}/>
+                {avatars.map((avatar: any) => (
+                  <div
+                    className={
+                      'container-avatar ' +
+                      (avatar.value === selected ? 'selected' : '') //verifica a imagem selecionada e aplica a classe css "selected" aplicando os estilos dessa classe
+                    }
+                    onClick={() => setSelected(avatar.value)}
+                    onDoubleClick={() => { //seleciona a imagem con doubleClick
+                      getAvatarUrl(avatar.value)//Captura imagem a ser utilizada  
+                      submitAvatar()//utiliza a imagem seleciona como o avatar
+                      setShowModal(false)// fecha o modal ao finaliza a seleção da imagem
+                    }}
 
-               </div>
-               
-               )}
+                    key={avatar.value}
+                  >
+                    <img src={getAvatarUrl(avatar.value)} />
+                  </div>
+                ))}
               </div>
             </div>
             <div className="actions">
               <span onClick={() => setShowModal(false)}>Voltar</span>
-              <button>Salvar</button>
+              <button type="button" onClick={submitAvatar}>
+                Salvar
+              </button>
             </div>
           </div>
         </Modal.Body>
--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -1,6 +1,7 @@
 import { PublicInput } from '../component/general/PublicInput'
 import { AvatarInput } from '../component/general/Avatarinput'
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 import logo from '../assets/images/logo.svg'
 import userIcon from '../assets/images/user.svg'
@@ -8,46 +9,60 @@ import emailIcom from '../assets/images/mail.svg'
 import passwordIcon from '../assets/images/key.svg'
 
 export const Register = () => {
+
+  const [image, setImage]= useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
   return (
     <>
       <div className="container-public register">
         <img src={logo} alt="logo devameet" className="logo" />
         <form>
-          <AvatarInput />
-          <p className="error"> Favor preencher os campos</p>
+          <AvatarInput
+          image={image}
+          setImage={setImage}
+          
+          
+          />
+          { error && <p className="error">{error}</p>}
           <PublicInput
             icon={userIcon}
             name="Nome"
             alt="Nome"
-            modelValue={''}
+            modelValue={name}
             type="text"
-            setValue={() => {}}
+            setValue={() => {setName}}
           />
           <PublicInput
             icon={emailIcom}
             name="Email"
             alt="Email"
-            modelValue={''}
+            modelValue={email}
             type="email"
-            setValue={() => {}}
+            setValue={() => {setEmail}}
           />
           <PublicInput
             icon={passwordIcon}
             name="Senha"
             alt="Senha"
-            modelValue={''}
+            modelValue={password}
             type="password"
-            setValue={() => {}}
+            setValue={() => {setPassword}}
           />
           <PublicInput
             icon={passwordIcon}
             name="Confirme a senha"
             alt="Confirme a Senha"
-            modelValue={''}
+            modelValue={confirm}
             type="password"
-            setValue={() => {}}
+            setValue={() => {setConfirm}}
           />
-          <button type="button">Cadastrar</button>
+          <button type='button' disabled={loading} > {loading ? '...Loading' : 'Cadastrar' }</button>
           <div className="link">
             <p> Já possui uma conta?</p>
             <Link to="/"> Faça seu login agora</Link>
