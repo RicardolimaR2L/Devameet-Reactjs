d00f88aa5018ee38cabd7a99c98abedfbcd86fe6
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-09-07 12:36:54 -0300

    Footer Parte 2

--- src/component/general/Navigation.tsx
+++ src/component/general/Navigation.tsx
@@ -8,7 +8,19 @@ import { useLocation, useNavigate } from 'react-router-dom'
 export const Navigation = () => {
 
   const navigate = useNavigate()
-  const location = useLocation() //Hook que nos retorna a url em que estamos
+  const location = useLocation() //Hook que nos retorna a url em que estamos.
+
+  const mobile = window.innerWidth <= 992;// atribiu um tamanho máximo para ser considerado mobile.
+
+  const avatarImage = ()=>{ //função para pegar o avatar que o usuario escolheu.
+    const avatar = localStorage.getItem('avatar'); //pega a imagem do localStorage.
+    if(avatar){ 
+      const path = `../../assets/objects/avatar/${avatar}_front.png` // path é o caminho de onde vem a imagem, aqui estamos fazendo a interpolação com a variavél avatar para pegar a imagem correta de acordo com o descrito no localStorage.
+      const imageUrl = new URL(path, import.meta.url); //pega o caminho e a importação e converte para uma nova url, isso vai trazer a imagem
+      return imageUrl.href
+    }
+    return avatarIcon //Se não tivermos um avatar selecionado pelo usuário, retorna somente a imagem de avatar padrão.
+   }
 
   const getIcon = (name: string) => {
     switch (name) {
@@ -49,20 +61,28 @@ export const Navigation = () => {
               onClick={() => navigate('/')}
             />
           </li>
-
-          <li>
+              {mobile ?  <li> {/* if para decidir como será renderizado o icone de entra na reunião , no mobile com click, no desktop sem click.*/}
             <img
               src={getIcon('room')}
               alt="Entrar na reunião"
               className="iconeNav"
               onClick={() => navigate('/link')}
             />
+          </li> : 
+          
+          <li className='disabled'>
+            <img
+              src={getIcon('room')}
+              alt="Entrar na reunião"
+              className="iconeNav"
+            />
           </li>
+          }
 
           <li>
             <div className={"avatar mini " + getSelectedClass()} >
               <img
-                src={avatarIcon}
+                src={avatarImage()} //Aqui ocorre a chamada função para atualizar o avatar do usuário no menu de navegação.
                 alt="Editar usuário"
                 onClick={() => navigate('/user')}
               />
