11f0a8f7722c5d2f74aa6ce70fae2df9f37fe96e
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-09-22 23:09:16 -0300

    Adição de reuniões - Parte 3 ( aula 5 parte 7 )

--- src/component/meet/Meetadd.tsx
+++ src/component/meet/Meetadd.tsx
@@ -12,7 +12,22 @@ export const MeetAdd = () => {
         </div>
         <div className="container-objects">
           <div className="center">
-            <div className="grid"></div>
+            <div className="grid">
+              <div className="line row one" />
+              <div className="line row two" />
+              <div className="line row three" />
+              <div className="line row four" />
+              <div className="line row five" />
+              <div className="line row six" />
+              <div className="line row seven" />
+              <div className="line column one" />
+              <div className="line column two" />
+              <div className="line column three" />
+              <div className="line column four" />
+              <div className="line column five" />
+              <div className="line column six" />
+              <div className="line column seven" />
+            </div>
             <div className="actions">
               <div>
                 <img src={trashIcon} alt="trash" />
